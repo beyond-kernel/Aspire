db79f7e787bcea9edaf786458fce970e8ac12ba7
Rafael <rafanog.linux@gmail.com>
2023-11-20 20:50:24 -0300

    inclusao do container default do rabbitmq

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -2,10 +2,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("cache");
 
+var rabbitmq = builder.AddRabbitMQContainer("rabbitmq");
+
 var apiservice = builder.AddProject<Projects.Aspire_ApiService>("apiservice");
 
 builder.AddProject<Projects.Aspire_Web>("webfrontend")
       .WithReference(cache)
+      .WithReference(rabbitmq)
     .WithReference(apiservice);
 
 builder.Build().Run();
